b62621aaf9ee9deac66942ea643f5cd84425c627
Ar1f007 <muhammadarif9416@yahoo.com>
2022-08-09 21:28:12 +0600

    comment reply logic

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,15 +3,27 @@ import { FaEdit, FaHeart, FaReply, FaTrash } from 'react-icons/fa';
 import { usePost } from '../context/PostContext';
 import { CommentList } from './CommentList';
 import { useState } from 'react';
+import { CommentForm } from './CommentForm';
+import { createComment } from '../services/comments';
+import { useAsyncFn } from '../hooks/useAsync';
 
 const dateFormatter = new Intl.DateTimeFormat(undefined, {
   dateStyled: 'medium',
   timeStyle: 'short',
 });
 export const Comment = ({ id, message, user, createdAt }) => {
-  const { getReplies } = usePost();
+  const { post, getReplies, createLocalComment } = usePost();
   const childComments = getReplies(id);
   const [areChildrenHidden, setAreChildrenHidden] = useState(false);
+  const [isReplying, setIsReplying] = useState(false);
+  const createCommentFn = useAsyncFn(createComment);
+
+  const onCommentReply = (message) => {
+    return createCommentFn.execute({ postId: post.id, message, parentId: id }).then((comment) => {
+      createLocalComment(comment);
+      setIsReplying(false);
+    });
+  };
   return (
     <>
       <div className="comment">
@@ -25,11 +37,27 @@ export const Comment = ({ id, message, user, createdAt }) => {
           <IconBtn Icon={FaHeart} aria-label="Like">
             2
           </IconBtn>
-          <IconBtn Icon={FaReply} aria-label="Reply"></IconBtn>
+          <IconBtn
+            isActive={isReplying}
+            onClick={() => setIsReplying((prev) => !prev)}
+            Icon={FaReply}
+            aria-label={isReplying ? 'Cancel reply' : 'Reply'}
+          ></IconBtn>
           <IconBtn Icon={FaEdit} aria-label="Edit"></IconBtn>
           <IconBtn Icon={FaTrash} aria-label="Delete" color="danger" />
         </div>
       </div>
+
+      {isReplying && (
+        <div className="mt-1 ml-3">
+          <CommentForm
+            autoFocus
+            onSubmit={onCommentReply}
+            loading={createCommentFn.loading}
+            error={createCommentFn.error}
+          />
+        </div>
+      )}
       {childComments?.length > 0 && (
         <>
           <div className={`nested-comments-stack ${areChildrenHidden ? 'hide' : ''}`}>
